a9c478cfdf3c37cb3ee6639e3c486f7bc867f128
Ramanpreet Chand <rchand595@gmail.com>
2017-08-24 22:21:33 -0400

    added extra credit and refactored code

--- ArithmeticCalculator/ArithmeticCalculator.cs
+++ ArithmeticCalculator/ArithmeticCalculator.cs
@@ -20,45 +20,56 @@ You can calculate a number raised to a power using the Math.Pow method.
          {
              while(true)
              {
-                Console.WriteLine("Give me a number.");
-                var ans1 = Console.ReadLine();
-                Console.WriteLine("Give me another number.");
-                var ans2 = Console.ReadLine();
                 Console.WriteLine("Pick an operation (1-4):");
                 Console.WriteLine("1. +");
                 Console.WriteLine("2. -");
                 Console.WriteLine("3. *");
                 Console.WriteLine("4. /");
+                Console.WriteLine("5. ^");
                 Console.WriteLine("0. Exit");
                 var ans3 = Console.ReadLine();
                 int opr = int.Parse(ans3);
-                int num1 = int.Parse(ans1);
-                int num2 = int.Parse(ans2);
-                int total = 0;
-                if(opr == 1)
-                {
-                    total = num1 + num2;
-                    Console.WriteLine("Total: " + total);
-                }
-                else if(opr == 2)
-                {
-                    total = num1 - num2;
-                    Console.WriteLine("Total: " + total);
-                }
-                else if(opr == 3)
-                {
-                    total = num1 * num2;
-                    Console.WriteLine("Total: " + total);
-                }
-                else if(opr == 4)
-                {
-                    total = num1 / num2;
-                    Console.WriteLine("Total: " + total);
-                }
-                else
+
+                if (opr == 0)
                 {
                     Console.WriteLine("Goodbye.");
                     break;
+                }
+                else
+                {
+                    Console.WriteLine("Give me a number.");
+                    var ans1 = Console.ReadLine();
+                    Console.WriteLine("Give me another number.");
+                    var ans2 = Console.ReadLine();
+
+                    double num1 = double.Parse(ans1);
+                    double num2 = double.Parse(ans2);
+                    var total = 0.0;
+                    if(opr == 1)
+                    {
+                        total = num1 + num2;
+                        Console.WriteLine("Total: " + total);
+                    }
+                    else if(opr == 2)
+                    {
+                        total = num1 - num2;
+                        Console.WriteLine("Total: " + total);
+                    }
+                    else if(opr == 3)
+                    {
+                        total = num1 * num2;
+                        Console.WriteLine("Total: " + total);
+                    }
+                    else if(opr == 4)
+                    {
+                        total = num1 / num2;
+                        Console.WriteLine("Total: " + total);
+                    }
+                    else if(opr == 5)
+                    {
+                        total = Math.Pow(num1, num2);
+                        Console.WriteLine("Total: " + total);
+                    }
                 }
              }
          }
